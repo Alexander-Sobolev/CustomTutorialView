7588c6f31d35485850f94aa58677375b4c8e4fa3
Alexander-Sobolev <sobolev.ios.developer@gmail.com>
2022-04-01 09:40:03 +0300

    Initial Commit

---
+++ CustomPageView/CustomPageViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomPageViewApp.swift
+//  CustomPageView
+//
+//  Created by Alexander Sobolev on 01.04.2022.
+//
+
+import SwiftUI
+
+@main
+struct CustomPageViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
